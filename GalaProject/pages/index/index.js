af814479e4c3c389cf3e9f190ea8e008b9d7baa4
palinpaalin <32041018+palinpaalin@users.noreply.github.com>
2018-12-23 02:30:08 -0500

    Initialize Timeline

--- GalaProject/pages/index/index.js
+++ GalaProject/pages/index/index.js
@@ -43,6 +43,13 @@ Page({
       })
     }
   },
+
+  Timeline: function () {
+    wx.navigateTo({
+      url: '../Timeline',
+    })
+  },
+
   getUserInfo: function(e) {
     console.log(e)
     app.globalData.userInfo = e.detail.userInfo
